6c178e56f2c06ef193d0b2932c1e9c6511bac99b
Marco Cecconi <marco.cecconi@gmail.com>
2013-10-12 14:37:24 +0100

    donate food

---
+++ FreeFoodUs/Controllers/DonateController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FreeFoodUs.Controllers
+{
+    public class DonateController : Controller
+    {
+        //
+        // GET: /Donate/
+
+        
+        public ActionResult Food()
+        {
+            return View();
+        }
+
+    }
+}
