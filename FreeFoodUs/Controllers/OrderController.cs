cf4d6fad222f5cc1330709797137030b8076c967
Marco Cecconi <marco.cecconi@gmail.com>
2013-10-13 10:21:52 +0100

    order completion

---
+++ FreeFoodUs/Controllers/OrderController.cs
@@ -0,0 +1,62 @@
+﻿using System.Web.Mvc;
+using FreeFoodUs.Models;
+using FreeFoodUs.Views.Shared;
+
+namespace FreeFoodUs.Controllers
+{
+    public class OrderController : Controller
+    {
+        public ActionResult Get(int id)
+        {
+            var u = Session["User"] as User;
+            var p = Session["Provider"] as Provider;
+
+            if (u == null && p == null)
+                return HttpNotFound();
+
+            Order model = Order.Get(id);
+
+            if (u != null)
+            {
+                if (model.UserId != u.Id)
+                {
+                    return HttpNotFound();
+                }
+                else
+                {
+                    return View("~/Views/Order/Get.cshtml", model);
+                }
+            }
+
+            if (model.ProviderId != p.Id)
+            {
+                return HttpNotFound();
+            }
+
+            return View("~/Views/Order/Provider.cshtml", model);
+        }
+
+        [HttpPost]
+        public ActionResult Delivered(int id)
+        {
+            var p = Session["Provider"] as Provider;
+            if (p == null)
+            {
+                return HttpNotFound();
+            }
+            Order.Complete(id, p.Id);
+            return View("~/Views/Shared/Plain.cshtml", new PlainModel { Title = "Thank you!", Text = "The order has been marked as complete." });
+        }
+
+        public ActionResult List()
+        {
+            var p = Session["Provider"] as Provider;
+            if (p == null)
+            {
+                return HttpNotFound();
+            }
+            var orders = Order.GetByProvider(p.Id);
+            return View(orders);
+        }
+    }
+}
--- FreeFoodUs/Models/Order.cs
+++ FreeFoodUs/Models/Order.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data.SqlClient;
+using System.Linq;
 using System.Web.Helpers;
 using Dapper;
 
@@ -14,15 +15,56 @@ namespace FreeFoodUs.Models
         public int UserId { get; set; }
         public List<FoodStock> Food { get; set; }
         public BankTransaction BuyOnPremise { get; set; }
+        public string JsonBody { get; set; }
 
         public void Execute()
         {
             using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["Main"].ConnectionString))
             {
-                connection.Execute(
-                    @"INSERT Orders (Date, JsonBody, ProviderId, UserId) VALUES (@Date, @Description, @ProviderId, @UserId)",
-                    new { Date = DateTime.UtcNow, Description = Json.Encode(new { Food, BuyOnPremise }), ProviderId, UserId });
+                Id = connection.Query<int>(
+                    @"INSERT Orders (Date, JsonBody, ProviderId, UserId) VALUES (@Date, @Description, @ProviderId, @UserId); SELECT CAST(SCOPE_IDENTITY() as int)",
+                    new { Date = DateTime.UtcNow, Description = Json.Encode(new InnerData { Food = Food, BuyOnPremise = BuyOnPremise }), ProviderId, UserId }).Single();
             }
         }
+
+        public static Order Get(int id)
+        {
+            using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["Main"].ConnectionString))
+            {
+                var res = connection.Query<Order>(
+                    @"SELECT * FROM Orders WHERE Id=@Id",
+                    new { Id = id }).SingleOrDefault();
+                var extra = Json.Decode<InnerData>(res.JsonBody);
+                res.Food = extra.Food;
+                res.BuyOnPremise = extra.BuyOnPremise;
+                return res;
+            }
+        }
+
+        public static void Complete(int id, int providerId)
+        {
+            using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["Main"].ConnectionString))
+            {
+                connection.Execute(
+                    @"Update Orders Set Processed=1 where id=@Id and ProviderId=@ProviderId", new { Id = id, ProviderId = providerId });
+            }
+        }
+
+        public static List<Order> GetByProvider(int id)
+        {
+            using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["Main"].ConnectionString))
+            {
+                var res = connection.Query<Order>(
+                    @"SELECT * FROM Orders WHERE ProviderId=@Id and Processed=0",
+                    new { Id = id }).ToList();
+                return res;
+            }
+        }
+    }
+
+    public class InnerData
+    {
+        public List<FoodStock> Food { get; set; }
+        public BankTransaction BuyOnPremise { get; set; }
     }
 }
