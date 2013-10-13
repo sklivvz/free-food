f766f737ed1b31041b87237bd033b804253bd1ea
Dave Hillier <dave.hillier1@gmail.com>
2013-10-13 06:13:19 +0100

    changed upper case food items from cached version

--- FreeFoodUs/Models/Provider.cs
+++ FreeFoodUs/Models/Provider.cs
@@ -10,6 +10,10 @@ namespace FreeFoodUs.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public float Lat { get; set; }
+        public float Lng { get; set; }
+        public string ImageUrl { get; set; }
+
 
         public static List<Provider> All()
         {
