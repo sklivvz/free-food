b3707bf83965c1f0b43033c38d0d5f704b7fc472
Marco Cecconi <marco.cecconi@gmail.com>
2013-10-13 05:27:17 +0100

    added users

---
+++ FreeFoodUs/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using Dapper;
+
+namespace FreeFoodUs.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public static List<User> All()
+        {
+            using (var connection = new SqlConnection(ConfigurationManager.ConnectionStrings["Main"].ConnectionString))
+            {
+                return connection.Query<User>(@"SELECT * FROM Users").ToList();
+            }
+        }
+    }
+}
---
+++ FreeFoodUs/Views/Actors/ActorsModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using FreeFoodUs.Models;
+
+namespace FreeFoodUs.Views.Actors
+{
+    public class ActorsModel
+    {
+        public List<Provider> Providers { get; set; }
+        public List<User> Users { get; set; }
+    }
+}
